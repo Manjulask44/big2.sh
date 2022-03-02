c91ebabeecf03f5c9507f2004509a984b882c80d
Manjulask44 <100059893+Manjulask44@users.noreply.github.com>
2022-03-02 17:58:54 +0530

    Update big2.sh
    script

--- big2.sh
+++ big2.sh
@@ -1,22 +1,7 @@
-#include <stdio.h>
-biggest()
-{
-    int num1, num2;
-    // Ask user to enter the two numbers
-    printf("Please Enter Two different values\n");
-    // Read two numbers from the user
-    scanf("%d %d", &num1, &num2);
-    if(num1 > num2)
-    {
-        printf("%d is Largest\n", num1);
-    }
-    else if (num2 > num1)
-    {
-        printf("%d is Largest\n", num2);
-    }
-    else
-    {
-        printf("Both are Equal\n");
-    }
-   // return 0;
-}
+#!/bin/bash
+
+if [ $1 -gt $2 ];then
+echo "$1 is big"
+else
+echo "$2 is big"
+fi
